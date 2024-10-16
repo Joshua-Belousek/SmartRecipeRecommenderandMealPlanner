8c9eea3e61cc2bb0c1ee93e46c07b5bcd3385e33
BilalMTaleb <bilalmtaleb@outlook.com>
2024-10-16 19:01:57 -0400

    updated main with input and output fiunctions and a simple test

--- main.py
+++ main.py
@@ -3,14 +3,51 @@ import json
 
 def read_ingredients_csv(file):
     with open(file, mode = 'r') as file: #open the file on reading mode
-        read = csv.reader(file)
-        next(read) # skip the header of the file
-        for row in read:
-            print(f"Name:{row[0]}, Quantity: {row[1]}, Unit:{row[2]}")
+        ingredients = []
+        reader = csv.reader(file)
+        next(reader) # skip the header of the file
+        for row in reader:
+            ingredients.append({
+                "name": row[0],     
+                "quantity": row[1],
+                "unit": row[2]})
+                  
+    return ingredients      #ingredients[0]['name'] will access the name of the first ingredient
 
 #function to read Json file
 def read_Json(file):
     with open(file, mode='r') as file:
-        data = json.load(file)
+        prefrences = json.load(file)
+    return prefrences
 
-read_ingredients_csv('ingredients.csv')
+
+# write the meal plan and shopping list to a text file considering meal_plan a list of stirngs
+# and shopping_list a list of dictonaries
+def output_meal_plan_Shopping_list(file_path, meal_plan, shopping_list):
+    with open(file_path, 'w') as file:  #open the file on writing mode
+        file.write("Meal Plan:\n")
+        for meal in meal_plan:
+            file.write(f"- {meal}\n")
+        
+        file.write("\nShopping List:\n")
+        for item in shopping_list:
+            file.write(f"- {item['quantity']} {item['unit']} {item['name']}\n")
+
+#simple test for output uncomment to test
+'''
+meal_plan = [
+    "Breakfast: Oatmeal with fruits",
+    "Lunch: Grilled Chicken Salad",
+    "Dinner: Salmon with vegetables"
+]
+
+shopping_list = [
+    {"name": "Chicken", "quantity": "600", "unit": "grams"},
+    {"name": "Oats", "quantity": "200", "unit": "grams"},
+    {"name": "Salmon", "quantity": "500", "unit": "grams"},
+    {"name": "Vegetables", "quantity": "300", "unit": "grams"}
+]
+
+output_meal_plan_Shopping_list("meal_plan_and_shopping_list.txt", meal_plan, shopping_list) 
+
+'''
