bf4ad0bb8a0ab3f84574ee8cb2b52cdbd5f8bb93
BilalMTaleb <bilalmtaleb@outlook.com>
2024-10-16 16:09:07 -0400

    Functions read Json and CSV

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 def read_ingredients_csv(file):
     with open(file, mode = 'r') as file: #open the file on reading mode
@@ -6,5 +7,10 @@ def read_ingredients_csv(file):
         next(read) # skip the header of the file
         for row in read:
             print(f"Name:{row[0]}, Quantity: {row[1]}, Unit:{row[2]}")
+
+#function to read Json file
+def read_Json(file):
+    with open(file, mode='r') as file:
+        data = json.load(file)
 
 read_ingredients_csv('ingredients.csv')
